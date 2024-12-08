b0a97aa98e2e3a9976564db8eb0aee6968cef49a
Zach Glossip <zglossip@gmail.com>
2024-12-08 13:47:47 -0600

    Finish Event resolvers

---
+++ src/main/java/com/zglossip/thrive/resolvers/EventMutationResolver.java
@@ -0,0 +1,44 @@
+package com.zglossip.thrive.resolvers;
+
+import com.zglossip.thrive.domains.Event;
+import com.zglossip.thrive.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDate;
+
+@Controller
+public class EventMutationResolver {
+
+    private final EventService eventService;
+
+    @Autowired
+    public EventMutationResolver(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @MutationMapping
+    public Event upsertEvent(
+            @Argument String id,
+            @Argument String description,
+            @Argument Integer startTime,
+            @Argument Integer endTime,
+            @Argument String date
+    ) {
+        return eventService.upsertEvent(new Event(
+                id,
+                description,
+                startTime,
+                endTime,
+                LocalDate.parse(date)
+        ));
+    }
+
+    @MutationMapping
+    public boolean deleteEvent(@Argument String id) {
+        return eventService.deleteEvent(id);
+    }
+
+}
---
+++ src/main/java/com/zglossip/thrive/resolvers/EventQueryResolver.java
@@ -0,0 +1,29 @@
+package com.zglossip.thrive.resolvers;
+
+import com.zglossip.thrive.domains.Event;
+import com.zglossip.thrive.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class EventQueryResolver {
+
+    private final EventService eventService;
+
+    @Autowired
+    public EventQueryResolver(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @QueryMapping
+    public List<Event> searchEventsByDate(@Argument String date) {
+        LocalDate parsedDate = LocalDate.parse(date);
+        return eventService.searchEventsByDate(parsedDate);
+    }
+
+}
